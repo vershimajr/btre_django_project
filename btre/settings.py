43923c02cc1cf59969b7a991b38cdae378144746
Amos Ajo <vershimaajo@gmail.com>
2019-05-20 04:04:49 -0400

    Remove Email Settings

--- btre/settings.py
+++ btre/settings.py
@@ -141,11 +141,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vershimaajo@gmail.com'
-EMAIL_HOST_PASSWORD = 'Amosajo1'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
