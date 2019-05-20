f60ba44420b4527e8b29662846c6b52cf2ca11f4
Amos Ajo <vershimaajo@gmail.com>
2019-05-20 03:58:20 -0400

    Added Local Settings

--- btre/settings.py
+++ btre/settings.py
@@ -146,3 +146,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vershimaajo@gmail.com'
 EMAIL_HOST_PASSWORD = 'Amosajo1'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
